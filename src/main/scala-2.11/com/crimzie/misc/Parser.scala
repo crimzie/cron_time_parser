fecec634fe31f4aebfa3769dd4edcc8dc3eb9690
Ivan Crimson <crimzie@gmail.com>
2016-10-27 01:07:59 +0300

    refactoring

--- src/main/scala-2.11/com/crimzie/misc/Parser.scala
+++ src/main/scala-2.11/com/crimzie/misc/Parser.scala
@@ -26,32 +26,33 @@ object Parser extends App {
   }
 
   val file: String = args(0)
-  val startTime: LocalTime = LocalTime.parse(this.args(1))
-  val endTime: LocalTime = LocalTime.parse(this.args(2))
+  val startTime: LocalTime = LocalTime parse this.args(1)
+  val endTime: LocalTime = LocalTime parse this.args(2)
 
   implicit val codec: Codec =
     UTF8
-    .onMalformedInput(REPLACE)
-    .onUnmappableCharacter(REPLACE)
+      .onMalformedInput(REPLACE)
+      .onUnmappableCharacter(REPLACE)
 
   val skipped =
     Source.fromFile(file)
       .getLines
       .filter(_.nonEmpty)
       .sliding(3, 3)
-      .map(x => {
+      .map { x =>
         val fields: Array[String] =
           x(2)
             .split(" ")
             .filter(_.nonEmpty)
-        (x.head,
+        (
+          x.head,
           x(1).split("=")(1), // timezone
-          fields(1),          // hours
-          fields(0),          // minutes
+          fields(1), // hours
+          fields(0), // minutes
           fields.drop(6).mkString(" "))
-      })
-      .filter(x => x._3.head != '*' && x._4.head != '*')
-      .map(x => {
+      }
+      .withFilter { x => x._3.head != '*' && x._4.head != '*' }
+      .map { x =>
         val offset: Int = TimeZone.getTimeZone(x._2).getRawOffset / 1000
         val time: Int = (x._3.toInt * 60 + x._4.toInt) * 60
         val locStart: Int = startTime.plusSeconds(offset).toSecondOfDay
@@ -60,17 +61,13 @@ object Parser extends App {
         val inTimeFrame: Boolean = locStart < time && locEnd >= time
         val skipped: Boolean = (!daySpan && inTimeFrame) || (daySpan && !inTimeFrame)
         (s" ${x._1}\n  ${x._5}", skipped)
-      })
-      .filter(_._2)
-      .toList
+      }
+      .withFilter(_._2)
+      .map(_._1)
 
-  if (skipped.nonEmpty) {
-    println(s"In file: $file\n" +
-      s"${skipped.size} tasks were missed:")
-    skipped.foreach(x => println(x._1))
-  } else {
-    println(s"In file: $file\n" +
-      s"no tasks were missed.")
-  }
-
+  println(s"In file: $file\n")
+  if (skipped hasNext) {
+    println(s"${skipped size} tasks were missed:")
+    skipped foreach println
+  } else println("no tasks were missed.")
 }
